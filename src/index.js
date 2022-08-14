1a33f5915a06772ce98b2dbf251b70777aee29b9
Patipat Chewprecha <patipat.che@mahidol.edu>
2022-08-14 14:02:25 +0700

    google authen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import * as Swal from 'sweetalert2'
 import { query, collection, getDocs, where } from 'firebase/firestore'
 import db from './firebaseconfig'
+import provider from './auth_google'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 console.log(db)
 
 // login form
@@ -56,4 +58,77 @@ async function login () {
       text: 'Error'
     })
   }
+}
+// btn Google Auth
+const btnGoogleAuth = document.getElementById('btnGGLogin')
+btnGoogleAuth.addEventListener('click', GGAuth)
+function GGAuth () {
+  const auth = getAuth()
+  signInWithPopup(auth, provider)
+    .then(async (result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result)
+      const token = credential.accessToken
+      // The signed-in user info.
+      const user = result.user
+      console.log(token)
+      console.log(user)
+      Swal.fire({
+        title: 'Gethering Data.....',
+        didOpen: () => {
+          Swal.showLoading()
+        }
+      })
+      try {
+        const q = query(
+          collection(db, 'user'),
+          where('email', '==', user.email)
+        )
+        const querySnapshot = await getDocs(q)
+        if (!querySnapshot.empty) {
+          Swal.close()
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Found User'
+          }).then((result) => {
+            alert('Success')
+            window.location = './user.html'
+          })
+        } else {
+          Swal.close()
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Not Found User'
+          }).then((result) => {
+            window.location = './register.html'
+          })
+        }
+      } catch (e) {
+        console.error('Error document: ', e)
+        Swal.close()
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Error'
+        })
+      }
+    // ...
+    }).catch((error) => {
+    // Handle Errors here.
+      console.log(error)
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.customData.email
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error)
+      Swal.close()
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Error'
+      })
+    })
 }
